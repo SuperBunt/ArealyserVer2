526d0ce8f0f150f3439a68d9a42737b8382de39f
Peter Connell <peterdconnell@hotmail.com>
2016-12-07 15:00:32 +0000

    Initial commit

---
+++ LocalVersion/envLocalVersion/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Dev\LocalVersion\LocalVersion\envLocalVersion\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
